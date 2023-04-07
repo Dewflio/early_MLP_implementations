8be3ed194bf2ee4041138bf76793be788160632f
Dewflio <43574262+Dewflio@users.noreply.github.com>
2023-04-07 15:03:06 +0200

    dataloader

---
+++ pre-trained/resnet_dataloader/resnet_dataloader.py
@@ -0,0 +1,8 @@
+# from: https://huggingface.co/datasets/imagenet-1k
+
+from classes import IMAGENET2012_CLASSES
+class ResnetDataLoader:
+    def __init__():
+        pass
+    def load_data(path):
+        pass
